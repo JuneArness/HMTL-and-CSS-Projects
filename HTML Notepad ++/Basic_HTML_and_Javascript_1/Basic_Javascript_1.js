3ba0a9d0b1020aab531bf9433b57afbe829d874f
JuneArness <junior@nationwidesoftware.com>
2021-06-07 13:25:24 -0400

    this is my first commit using Github desktop

---
+++ HTML Notepad ++/Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -0,0 +1,10 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> <script>
+    Function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green")
+	document.getElementById("Irish").innerHTML = 
+	result;
+)
+</script>
